a703d4136c005ef63976501057af304bcf0f142d
Marian Domanski <mmdski@gmail.com>
2019-09-01 15:49:59 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
-from setuptools import setup
-from setuptools.command.build_ext import build_ext as _build_ext
-
-try:
-    from Cython.Build import cythonize
-except ImportError:
-     def cythonize(*args, **kwargs):
-         from Cython.Build import cythonize
-         return cythonize(*args, **kwargs)
+from setuptools import setup, Extension
 
 setup(name='Hello world app',
-      setup_requires=['Cython'],
-      ext_modules=cythonize(['hello.pyx']),
+      setup_requires=['cython'],
+      ext_modules=[Extension('hello', sources=['hello.pyx'])],
       zip_safe=False)
